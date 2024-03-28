58fdfe62e62b1ffa25a60ced58c15398c1174d20
Njikem <123434988+Njikem@users.noreply.github.com>
2024-03-27 19:06:10 -0600

    variable

---
+++ js/app.js
@@ -0,0 +1,13 @@
+
+
+let name = 'john';
+console.log(name);
+name = 'Alex';
+console.log(name);
+let color = 'red';
+console.log(color);
+
+let numb1 = 9;
+console.log(numb1);
+let numb2 = 10;
+console.log(numb2);
